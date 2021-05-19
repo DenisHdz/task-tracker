b1448c5338b48bf9e29a3614955f36590c5459cf
Denis Hernandez <denishe.dev@gmail.com>
2021-05-19 12:28:21 +0100

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button
+      v-show="homePage"
+      @button-click="$emit('toggle-add-task')"
+      :text="displayForm ? 'Close' : 'Add'"
+      :color="displayForm ? 'Red' : 'Green'"
+    />
+  </header>
+</template>
+
+<script>
+import Button from "./Button";
+
+export default {
+  name: "Header",
+  props: {
+    title: String,
+    displayForm: Boolean,
+  },
+  components: {
+    Button,
+  },
+  computed: {
+    homePage() {
+      if (this.$route.path === "/") {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+};
+</script>
+
+<style>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
